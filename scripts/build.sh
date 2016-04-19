dc7274216011bc354d9cf201cf6178d0db376e1b
alexzaporozhets <alexzaporozhets@gmail.com>
2016-04-19 20:33:25 +0300

    Update build.sh
    build --no-cache

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,28 +4,28 @@ rm -r build; mkdir build; cd build
 
 # building API SERVER
 git clone https://github.com/mystaff/api-server; cd api-server
-docker build -t staff-api-server .
+docker build --no-cache -t staff-api-server .
 docker tag staff-api-server:latest 847166803921.dkr.ecr.us-east-1.amazonaws.com/staff-api-server:latest
 docker push 847166803921.dkr.ecr.us-east-1.amazonaws.com/staff-api-server:latest
 echo "API - DONE"; cd ..
 
 # building ADMIN APP
 git clone https://github.com/mystaff/app-admin; cd app-admin
-docker build -t staff-app-admin .
+docker build --no-cache -t staff-app-admin .
 docker tag staff-app-admin:latest 847166803921.dkr.ecr.us-east-1.amazonaws.com/staff-app-admin:latest
 docker push 847166803921.dkr.ecr.us-east-1.amazonaws.com/staff-app-admin:latest
 echo "ADMIN APP - DONE"; cd ..
 
 # building WEB APP
 git clone https://github.com/mystaff/app-web; cd app-web
-docker build -t staff-app-web .
+docker build --no-cache -t staff-app-web .
 docker tag staff-app-web:latest 847166803921.dkr.ecr.us-east-1.amazonaws.com/staff-app-web:latest
 docker push 847166803921.dkr.ecr.us-east-1.amazonaws.com/staff-app-web:latest
 echo "WEB APP - DONE"; cd ..
 
 # building DESKTOP APP
 git clone https://github.com/mystaff/app-desktop
-docker build -t staff-app-desktop .
+docker build --no-cache -t staff-app-desktop .
 docker tag staff-app-desktop:latest 847166803921.dkr.ecr.us-east-1.amazonaws.com/staff-app-desktop:latest
 docker push 847166803921.dkr.ecr.us-east-1.amazonaws.com/staff-app-desktop:latest
 echo "DESKTOP APP - DONE"; cd ..
